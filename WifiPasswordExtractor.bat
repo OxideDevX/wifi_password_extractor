65a433495828de5c6db60b48d9bb4c0bc7bf5258
Oxide Dev <59166229+OxideDevX@users.noreply.github.com>
2021-05-29 10:59:20 +0300

    Add files via upload

---
+++ WifiPasswordExtractor.bat
@@ -0,0 +1,14 @@
+@echo off
+rem Created OxideDevX....
+echo I'm working. Extracting Wi-Fi passwords.
+
+rem Delete old file(output)!!!
+del ssidswkeys.txt 2> nul
+for /F "tokens=4*" %%i in ('netsh wlan show profiles *^| findstr /l :"SSID name"') do ( 
+	
+	echo ssid: %%i %%j >> ssidswkeys.txt 
+	netsh wlan show profile name=%%i %%j key=clear | findstr /l :"Key Content" >> ssidswkeys.txt
+	echo( >> ssidswkeys.txt
+)
+echo The work has been completed. Passwords are extracted into a text file ssidswkeys.txt. All rights reserved. Developed by OxideDevX for Pentesters and Computer Forensics.
+pause
