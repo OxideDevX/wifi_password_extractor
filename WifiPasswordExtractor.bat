9936023ce88716ec641ecbf64d8430487a4dd0e8
Alex <59166229+OxideDevX@users.noreply.github.com>
2021-11-22 19:57:17 +0200

    update text

--- WifiPasswordExtractor.bat
+++ WifiPasswordExtractor.bat
@@ -6,9 +6,12 @@ rem Delete old file(output)!!!
 del ssidswkeys.txt 2> nul
 for /F "tokens=4*" %%i in ('netsh wlan show profiles *^| findstr /l :"SSID name"') do ( 
 	
-	echo ssid: %%i %%j >> ssidswkeysextraxt.txt 
+	echo ssid: %%i %%j >> ssidswkeysextraxt.txt
 	netsh wlan show profile name=%%i %%j key=clear | findstr /l :"Key Content" >> ssidswkeysextraxt.txt 
 	echo( >> ssidswkeysextraxt.txt 
 )
-echo The work has been completed. Passwords are extracted into a text file ssidswkeysextraxt.txt . All rights reserved. Developed by OxideDevX for Pentesters and Computer Forensics.
+echo The work has been completed. 
+echo Passwords are extracted into a text file ssidswkeysextraxt.txt . 
+echo All rights reserved. 
+echo It is script developed by OxideDevX for Pentesters and Computer Forensics. Happy day!
 pause
